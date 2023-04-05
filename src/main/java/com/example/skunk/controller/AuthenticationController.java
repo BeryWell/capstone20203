ed0f3cd24818dd2c02ead438a56d587535a3c955
kongji <kongji4092@gmail.com>
2023-04-05 22:28:41 +0900

    code review bot 테스트 코드 작성

--- src/main/java/com/example/skunk/controller/AuthenticationController.java
+++ src/main/java/com/example/skunk/controller/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.example.skunk.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,5 +13,10 @@ public class AuthenticationController {
     @PostMapping("/signup")
     public ResponseEntity signup(){
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity test(){
+        return ResponseEntity.ok("test success!");
     }
 }
