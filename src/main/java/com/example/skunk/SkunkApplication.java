1e0844c065f9b6234df5b2bc063bd04b3065d0f3
kongji <kongji4092@gmail.com>
2023-05-30 19:07:38 +0900

    bug-fix : "swagger 오류 해소"

--- src/main/java/com/example/skunk/SkunkApplication.java
+++ src/main/java/com/example/skunk/SkunkApplication.java
@@ -2,8 +2,10 @@ package com.example.skunk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SkunkApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/skunk/config/SwaggerConfig.java
+++ src/main/java/com/example/skunk/config/SwaggerConfig.java
@@ -2,14 +2,19 @@ package com.example.skunk.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket api() {
